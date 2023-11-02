887658c8c6671d96613a51d9425bb813a8fb59a2
hurk@jxgtmbjt.com <hurk@jxgtmbjt.com>
2023-11-02 14:13:45 +0800

    2023-11-2学习记录提交

--- rememberme-persis-2/src/main/java/org/javaboy/formlogin/SecurityConfig.java
+++ rememberme-persis-2/src/main/java/org/javaboy/formlogin/SecurityConfig.java
@@ -59,12 +59,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new PersistentTokenBasedRememberMeServices("javaboy123", userDetailsService(), jdbcTokenRepository());
     }
 
+    //持久化令牌的方式其实已经安全很多了，但是依然存在用户身份被盗用的问题，这个问题实际上很难完美解决，我们能做的，只能是当发生用户身份被盗用这样的事情时，将损失降低到最小,因此，我们来看下另一种方案，就是二次校验。
+
+    /**
+     * 为了让用户使用方便，我们开通了自动登录功能，但是自动登录功能又带来了安全风险，一个规避的办法就是如果用户使用了自动登录功能，我们可以只让他做一些常规的不敏感操作，
+     * 例如数据浏览、查看，但是不允许他做任何修改、删除操作，如果用户点击了修改、删除按钮，我们可以跳转回登录页面，让用户重新输入密码确认身份，然后再允许他执行敏感操作。
+     *
+     * 这个功能在 Shiro 中有一个比较方便的过滤器可以配置，Spring Security 当然也一样，例如我现在提供三个访问接口：
+     * 第一个 /hello 接口，只要认证后就可以访问，无论是通过用户名密码认证还是通过自动登录认证，只要认证了，就可以访问。
+     * 第二个 /admin 接口，必须要用户名密码认证之后才能访问，如果用户是通过自动登录认证的，则必须重新输入用户名密码才能访问该接口。
+     * 第三个 /rememberme 接口，必须是通过自动登录认证后才能访问，如果用户是通过用户名/密码认证的，则无法访问该接口。
+
+     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/rememberme").rememberMe()
-                .antMatchers("/admin").fullyAuthenticated()
-                .anyRequest().authenticated()
+                .antMatchers("/rememberme").rememberMe()///rememberme 接口是需要 rememberMe 才能访问。
+                .antMatchers("/admin").fullyAuthenticated()// admin 是需要 fullyAuthenticated，fullyAuthenticated 不同于 authenticated，fullyAuthenticated 不包含自动登录的形式，而 authenticated 包含自动登录的形式。
+                .anyRequest().authenticated()//最后剩余的接口（/hello）都是 authenticated 就能访问。
                 .and()
                 .formLogin()
                 .permitAll()
--- rememberme-persis/src/main/java/org/javaboy/formlogin/SecurityConfig.java
+++ rememberme-persis/src/main/java/org/javaboy/formlogin/SecurityConfig.java
@@ -37,6 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
+    //持久化令牌就是在基本的自动登录功能基础上，又增加了新的校验参数，来提高系统的安全性，这一些都是由开发者在后台完成的，对于用户来说，登录体验和普通的自动登录体验是一样的。
+
+    //提供一个 JdbcTokenRepositoryImpl 实例，并给其配置 DataSource 数据源，最后通过 tokenRepository 将 JdbcTokenRepositoryImpl 实例纳入配置中。
+
+    //在持久化令牌中，新增了两个经过 MD5 散列函数计算的校验参数，一个是 series，另一个是 token。其中，series 只有当用户在使用用户名/密码登录时，才会生成或者更新，而 token 只要有新的会话，就会重新生成，这样就可以避免一个用户同时在多端登录
+    // ，就像手机 QQ ，一个手机上登录了，就会踢掉另外一个手机的登录，这样用户就会很容易发现账户是否泄漏（之前看到松哥交流群里有小伙伴在讨论如何禁止多端登录，其实就可以借鉴这里的思路）。
     @Bean
     JdbcTokenRepositoryImpl jdbcTokenRepository() {
         JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
