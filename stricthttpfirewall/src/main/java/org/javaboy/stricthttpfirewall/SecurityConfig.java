fb7bba5498955945074b5f9e00a81e66d1e53560
hurk@jxgtmbjt.com <hurk@jxgtmbjt.com>
2023-11-02 17:53:00 +0800

    2023-11-2学习记录提交

--- stricthttpfirewall/src/main/java/org/javaboy/stricthttpfirewall/SecurityConfig.java
+++ stricthttpfirewall/src/main/java/org/javaboy/stricthttpfirewall/SecurityConfig.java
@@ -20,10 +20,21 @@ import java.util.Arrays;
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 从 StrictHttpFirewall 的源码中看出来： 你的 HTTP 请求方法必须是 DELETE、GET、HEAD、OPTIONS、PATCH、POST 以及 PUT 中的一个，请求才能发送成功，否则的话，就会抛出 RequestRejectedException 异常。
+
+     */
     @Bean
     HttpFirewall httpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
+        //setUnsafeAllowAnyHttpMethod 方法表示不做 HTTP 请求方法校验，也就是什么方法都可以过。或者也可以通过 setAllowedHttpMethods 方法来重新定义可以通过的方法。
         firewall.setAllowUrlEncodedPeriod(true);
+        //这四种路径中，都不能包含如下字符串：  "./", "/../" or "/."
+        firewall.setAllowUrlEncodedPercent(true);//希望请求地址中可以出现 %
+        firewall.setAllowUrlEncodedDoubleSlash(true);//希望请求地址中可以出现 //
+        firewall.setAllowUrlEncodedSlash(true);// 去掉 % 和 反斜杠的限制
+        firewall.setAllowUrlEncodedPeriod(true);//如果请求地址中存在 . 编码之后的字符 %2e、%2E，则请求将被拒绝   如需支持，按照如下方式进行配置
+
         return firewall;
     }
 
